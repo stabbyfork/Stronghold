c73971d06c365f838839eb2a45cc5429ce5f7fc8
stabbyfork <138290921+stabbyfork@users.noreply.github.com>
2025-12-09 23:36:08 +0200

    Prepare for implementation

--- src/models/blacklistedUser.ts
+++ src/models/blacklistedUser.ts
@@ -1,7 +1,9 @@
 import { CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model } from '@sequelize/core';
 import { Attribute, Table } from '@sequelize/core/decorators-legacy';
 
-// TODO: Make this way better and integrated
+// TODO: Migrate to RobloxUser, find user ID from Roblox username
+// /rbx blacklist add/remove/clear/list/find
+// TODO: Add Convenience commands that combine multiple commands
 @Table({ indexes: [{ unique: true, fields: ['guildId', 'username'] }] })
 export class BlacklistedUser extends Model<InferAttributes<BlacklistedUser>, InferCreationAttributes<BlacklistedUser>> {
 	@Attribute({ primaryKey: true, autoIncrement: true, type: DataTypes.INTEGER.UNSIGNED })
@@ -13,6 +15,9 @@ export class BlacklistedUser extends Model<InferAttributes<BlacklistedUser>, Inf
 
 	@Attribute({ type: DataTypes.STRING(50), allowNull: false })
 	declare username: string;
+
+	@Attribute({ type: DataTypes.STRING(128), allowNull: true })
+	declare blacklistReason: string | null;
 
 	@Attribute({ type: DataTypes.DATE })
 	declare createdAt: CreationOptional<Date>;

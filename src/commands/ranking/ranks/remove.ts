ab3913f6884fc4fb87565fe89a049e94992d5e07
stabbyfork <138290921+stabbyfork@users.noreply.github.com>
2026-05-26 21:35:24 +0300

    Add error message to rank removal

--- src/commands/ranking/ranks/remove.ts
+++ src/commands/ranking/ranks/remove.ts
@@ -27,13 +27,18 @@ export default async (interaction: ChatInputCommandInteraction, args: typeof com
 		await reportErrorToUser(interaction, constructError([ErrorReplies.RankNotFoundSubstitute], name), true);
 		return;
 	}
-	// May error
-	await Data.mainDb.transaction(async (transaction) => {
-		for (const user of await Data.models.User.findAll({ where: { mainRankId: rank.rankId }, transaction })) {
-			await Data.promoteUser(user);
-		}
-		await rank.destroy({ transaction });
-	});
+	try {
+		// May error
+		await Data.mainDb.transaction(async (transaction) => {
+			for (const user of await Data.models.User.findAll({ where: { mainRankId: rank.rankId }, transaction })) {
+				await Data.promoteUser(user);
+			}
+			await rank.destroy({ transaction });
+		});
+	} catch (error) {
+		await reportErrorToUser(interaction, `Failed to remove rank ${rank.name}. Error: ${error}`, true);
+	}
+
 	await interaction.reply({
 		embeds: [defaultEmbed().setTitle('Success').setDescription(`Removed rank ${rank.name}.`).setColor('Green')],
 	});

03e1c832cb995666f5f63075391b8827207d4172
stabbyfork <138290921+stabbyfork@users.noreply.github.com>
2026-06-06 19:23:25 +0300

    Use name instead of role mention in rank listing

--- src/commands/ranking/ranks/list.ts
+++ src/commands/ranking/ranks/list.ts
@@ -35,7 +35,7 @@ export default async (interaction: ChatInputCommandInteraction) => {
 							.slice(start, start + perPage)
 							.map(
 								(rank) =>
-									`${roleMention(rank.roleId)}: \`${rank.pointsRequired}\` points${rank.userLimit === -1 ? '' : `, \`${rank.rankUsage?.userCount ?? 0}\`/\`${rank.userLimit}\` user(s)`}${rank.stackable ? ' (stacking)' : ''}`,
+									`${rank.name}: \`${rank.pointsRequired}\` points${rank.userLimit === -1 ? '' : `, \`${rank.rankUsage?.userCount ?? 0}\`/\`${rank.userLimit}\` user(s)`}${rank.stackable ? ' (stacking)' : ''}`,
 							)
 							.join('\n'),
 					),
